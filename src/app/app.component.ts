8d6d521e9c3fd8a3d71632df0e8fdaa84bdf6cd8
Nicolas Navarro <nicolasnavarro@MacBookPro.local>
2025-04-24 15:25:27 -0300

    fix: remove useless component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MovieMapComponent } from './domains/movie-map/movie-map.component';
 import { SharedModule } from './shared/shared.module';
 import { RouterOutlet } from '@angular/router';
 import { LocationsComponent } from './domains/locations/locations.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SharedModule,LocationsComponent, MovieMapComponent ],
+  imports: [RouterOutlet,SharedModule,LocationsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { MovieMapComponent } from './domains/movie-map/movie-map.component';
 import { LocationsComponent } from './domains/locations/locations.component';
 
 export const routes: Routes = [
       {
     path: '',
     component: LocationsComponent, 
-  },
-  {
-    path: 'map',
-    component: MovieMapComponent, 
   },
 ];
