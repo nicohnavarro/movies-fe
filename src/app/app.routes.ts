b65faca1b86ea96381bc03487059f10a19f41010
Nicolas Navarro <nicolasnavarro@MacBookPro.local>
2025-04-25 17:19:18 -0300

    fix: ux ui details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './domains/home/home.component';
 import { LocationsComponent } from './domains/locations/locations.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: LocationsComponent,
   }, {
     path: 'v1',
     component: LocationsComponent,
